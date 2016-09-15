3dce050dca47af13f546c4ec314edeff8706faf9
Jussi Nummelin <jussi@kontena.io>
2016-09-15 14:59:49 +0300

    handle address in request. fixes #3
    handle given address in api
    
    revert reserved and available addresses to use strings

--- app/mutations/addresses/request.rb
+++ app/mutations/addresses/request.rb
@@ -5,32 +5,53 @@ module Addresses
     include Logging
 
     required do
-      string :pool_id, default: 'kontena'
+      string :pool_id
+    end
+
+    optional do
+      model :address, class: IPAddr
     end
 
     def validate
       resp = etcd.get("/kontena/ipam/pools/#{self.pool_id}") rescue nil
       add_error(:error, :not_found, 'Pool not found') if resp.nil?
-
-      @pool = resp.value
+      @pool = resp.value unless resp.nil?
+      if @pool && self.address
+        pool = IPAddr.new(@pool)
+        add_error(:error, :address_not_within_pool,
+          "Given address not within pool") unless pool.include?(self.address)
+      end
     end
 
     def execute
       addresses = available_addresses
-      info "requesting address in pool: #{@pool}"
+      info "requesting address(#{self.address}) in pool: #{@pool}"
       info "available (#{self.pool_id}): #{addresses.size}"
-      if addresses.size > 100
-        ip = addresses[rand(0..100)]
+      info "available: #{addresses.include?(self.address)}" if self.address
+      ip = nil
+      if self.address
+        if addresses.include?(self.address.to_s)
+          ip = self.address
+        else
+          add_error(:error, :not_available, 'Given address already taken')
+          return
+        end
       else
-        ip = addresses[0]
-      end
-      if ip
-        etcd.set("/kontena/ipam/addresses/#{self.pool_id}/#{ip}", value: ip)
-      else
-        add_error(:error, :cannot_allocate, 'Cannot allocate ip, address pool is full')
+        if addresses.size > 100
+          ip = addresses[rand(0..100)]
+        else
+          ip = addresses[0]
+        end
       end
 
-      "#{ip}/#{@pool.split('/')[1]}"
+      if ip
+        etcd.set("/kontena/ipam/addresses/#{self.pool_id}/#{ip}", value: ip.to_s)
+      else
+        add_error(:error, :cannot_allocate, 'Cannot allocate ip, address pool is full')
+        return
+      end
+
+      "#{ip.to_s}/#{@pool.split('/')[1]}"
     end
 
     # @return [Array<IPAddr>]
@@ -42,7 +63,7 @@ module Addresses
     def address_pool
       unless self.class.pools[@pool]
         if self.pool_id == 'kontena'
-          # In the default kontena network, skip first 255 addresses for weave expose
+          # In the default kontena network, skip first /24 block for weave expose
           self.class.pools[@pool] = IPAddr.new(@pool).to_range.to_a[256..-1].map{|ip| ip.to_s }
         else
           self.class.pools[@pool] = IPAddr.new(@pool).to_range.to_a[1..-1].map{|ip| ip.to_s }
@@ -55,7 +76,6 @@ module Addresses
     def reserved_addresses
       reserved_addresses = []
       response = etcd.get("/kontena/ipam/addresses/#{self.pool_id}/")
-      subnet_size = @pool.split('/')[1]
       response.children.map{|c|
         reserved_addresses << c.value
       }
