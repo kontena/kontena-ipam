e484ed01a2a47fbfa6fb5e5565a6c00e9e37d61c
Jussi Nummelin <jussi@kontena.io>
2016-10-04 15:49:36 +0300

    remove pool from etcd when only gw address left

--- app/mutations/address_pools/release.rb
+++ app/mutations/address_pools/release.rb
@@ -11,7 +11,19 @@ module AddressPools
     def validate
       unless @pool = AddressPool.get(pool_id)
         add_error(:pool_id, :notfound, "AddressPool not found: #{pool_id}")
+        return
       end
+
+      reserved_addresses = @pool.list_addresses
+      if reserved_addresses.size == 1
+        # Should be only the gw reserved
+        unless @pool.gateway.to_host == reserved_addresses.first.address.to_host
+          add_error(:pool, :gateway, "Expected gateway to be the only reserved address, instead had: #{reserved_addresses.first.address.to_host}")
+        end
+      else
+        add_error(:pool_id, :reserved_addresses, "AddressPool #{pool_id} has still #{reserved_addresses.size} reserved addresses")
+      end
+
     end
 
     def execute
--- spec/mutations/address_pools/release_spec.rb
+++ spec/mutations/address_pools/release_spec.rb
@@ -7,8 +7,9 @@ describe AddressPools::Release do
     end
 
     it 'accepts an existing network' do
-      expect(AddressPool).to receive(:get).with('kontena').and_return(AddressPool.new('kontena', subnet: IPAddr.new('10.80.0.0/16')))
-
+      pool = instance_double(AddressPool, id: 'kontena', subnet: IPAddr.new('10.80.0.0/16'), gateway: IPAddr.new('10.80.0.1/16'))
+      expect(AddressPool).to receive(:get).with('kontena').and_return(pool)
+      expect(pool).to receive(:list_addresses).and_return([double(pool: pool, id: '10.80.0.1', address: IPAddr.new('10.80.0.1/16'))])
       subject = described_class.new(pool_id: 'kontena')
 
       expect(subject).not_to have_errors, subject.validation_outcome.errors.inspect
@@ -24,16 +25,36 @@ describe AddressPools::Release do
       expect(outcome).to_not be_success
       expect(outcome.errors.symbolic[:pool_id]).to eq :notfound
     end
+
+    it 'rejects a network with reserved addresses' do
+      pool = instance_double(AddressPool, id: 'kontena', subnet: IPAddr.new('10.80.0.0/16'), gateway: IPAddr.new('10.80.0.1/16'))
+      expect(AddressPool).to receive(:get).with('kontena').and_return(pool)
+      expect(pool).to receive(:list_addresses)
+        .and_return(
+          [
+            double(pool: pool, id: '10.80.0.1', address: IPAddr.new('10.80.0.1/16')),
+            double(pool: pool, id: '10.80.0.2', address: IPAddr.new('10.80.0.2/16'))
+          ]
+        )
+
+      subject = described_class.new(pool_id: 'kontena')
+
+      outcome = subject.validation_outcome
+
+      expect(outcome).to_not be_success
+      expect(outcome.errors.symbolic[:pool_id]).to eq :reserved_addresses
+    end
   end
 
   describe '#execute' do
     context 'releasing a single pool' do
       let :pool do
-        AddressPool.new('kontena', subnet: IPAddr.new('10.80.0.0/16'))
+        AddressPool.new('kontena', subnet: IPAddr.new('10.80.0.0/16'), gateway: IPAddr.new('10.80.0.1/16'))
       end
 
       before do
         expect(AddressPool).to receive(:get).with('kontena').and_return(pool)
+        expect(pool).to receive(:list_addresses).and_return([double(pool: pool, id: '10.80.0.1', address: IPAddr.new('10.80.0.1/16'))])
       end
 
       it 'deletes the etcd node' do
