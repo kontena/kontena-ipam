aef44771ccf56d615013546bd24a3cd4530aafd1
Tero Marttila <tero.marttila@kontena.io>
2016-10-11 11:51:53 +0300

    Ectd::TestServer: Implement #modified? and #logs by tracking etcd indexes

--- lib/etcd/test_server.rb
+++ lib/etcd/test_server.rb
@@ -30,15 +30,46 @@ class Etcd::TestServer < Etcd::ServerBase
     # empty
   end
 
+  # Return current etcd index. This increments on each modification?
+  #
+  # @return [Integer]
+  def get_index
+    return @client.get(@root).etcd_index
+  rescue Etcd::KeyNotFound => error
+    return nil
+  end
+
+  # Uses etcd GET ?wait to yield modifications after from index up to and including to index
+  def get_logs(index, to)
+    # skip initial
+    index += 1
+
+    while index <= to
+      response = @client.watch(@root, recursive: true, waitIndex: index, timeout: 1)
+
+      yield response.action, response.node
+
+      index = response.node.modified_index + 1
+    end
+  rescue Net::ReadTimeout => error
+    fail "Unexpected end of watch stream at #{index} before #{to}"
+  end
+
   public
 
   # Clear the etcd server database.
   #
   # Used before the each test
   def reset!
-    @client.delete(@root, recursive: true)
+    response = @client.delete(@root, recursive: true)
+
+    # record initial reset index for modfied?
+    @etcd_reset = true
+    @etcd_index = response.etcd_index
+
   rescue Etcd::KeyNotFound => error
-    return
+    @etcd_reset = true
+    @etcd_index = error.index
   end
 
   # Load a hash of nodes into the store.
@@ -48,7 +79,11 @@ class Etcd::TestServer < Etcd::ServerBase
   # @param tree [Hash<String, Object or String>]
   def load!(tree)
     load_nodes(tree) do |key, value|
-      @client.set(key, value: value)
+      response = @client.set(key, value: value)
+
+      # record initial load index for modfied?
+      @etcd_reset = false
+      @etcd_index = response.etcd_index
     end
   end
 
@@ -58,11 +93,33 @@ class Etcd::TestServer < Etcd::ServerBase
   #
   # @return [Boolean]
   def modified?
-    # TODO: implememt
+    etcd_index = self.get_index
+
+    if @etcd_reset
+      # reset, not loaded, should not exist
+      return !etcd_index.nil?
+    elsif etcd_index.nil?
+      # reset, loaded, deleted
+      return true
+    else
+      # reset, loaded, still exists
+      return etcd_index > @etcd_index
+    end
   end
 
-  # Operation log
+  # Operations log
   def logs
-    # TODO: implememt
+    etcd_index = get_index
+
+    return nil if @etcd_index.nil? || etcd_index.nil?
+
+    logs = []
+    get_logs(@etcd_index, etcd_index) do |action, node|
+      path = node.key
+      path += '/' if node.directory?
+
+      logs << [action.to_sym, path]
+    end
+    logs
   end
 end
