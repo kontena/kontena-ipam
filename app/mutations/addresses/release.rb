4660bf3573854c5882023b167f2aae90ba5f051c
Jussi Nummelin <jussi@kontena.io>
2016-10-27 16:02:02 +0300

    fix address release after rebase

--- app/mutations/addresses/release.rb
+++ app/mutations/addresses/release.rb
@@ -23,8 +23,8 @@ module Addresses
 
     def execute
       info "releasing address: #{self.address} in pool: #{@pool_id}"
-      address = @pool.get_address(@address)
-      if address && address.to_host != @pool.gateway.to_host
+      address = @pool.get_address(self.address)
+      if address && address.address.to_host != @pool.gateway.to_host
         address.delete!
       end
     end
