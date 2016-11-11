b8fde654b88d3ab93685e32c850b687f15d37389
Jussi Nummelin <jussi@kontena.io>
2016-11-11 09:45:25 +0200

    keep docker network cleanup as alternative way of cleanup

--- app/address_cleanup.rb
+++ app/address_cleanup.rb
@@ -10,22 +10,31 @@ class AddressCleanup
   include Logging
   include NodeHelper
 
-  # @param node_id [String]
-  # @param reserver_addresses [Array<String>]
-  def initialize(reserved_addresses = [])
+  def initialize()
     @node = node
-    @reserved_addresses = reserved_addresses.map { |a| IPAddr.new(a).to_host }
   end
 
-  def cleanup
-    info "starting cleanup routine"
-    debug "locally known addresses: #{@reserved_addresses.size}"
+  def cleanup_docker_networks
+    cleanup(local_docker_known_addresses)
+  end
+
+  def cleanup_known_addresses(reserved_addresses = [])
+    cleanup(reserved_addresses.map { |a| IPAddr.new(a).to_host })
+  end
+
+  private
+
+  def cleanup(reserved_addresses)
+    info "starting cleanup routine for node: #{@node}"
+    debug "locally known addresses: #{reserved_addresses.size}"
     AddressPool.list.each { |pool|
       debug "checking pool: #{pool.id}"
       pool.list_addresses.each { |address|
         debug "checking address: #{address.address.to_host}..."
+        debug "address.node: #{address.node.inspect}"
+        debug "node: #{@node.inspect}"
         if address.node == @node
-          if @reserved_addresses.include?(address.address.to_host) || pool.gateway.to_host == address.address.to_host
+          if reserved_addresses.include?(address.address.to_host) || pool.gateway.to_host == address.address.to_host
             debug '..still in use or gateway, skipping.'
             next
           else
@@ -41,5 +50,28 @@ class AddressCleanup
     }
     info "cleanup done"
   end
+
+  # Collect all known addresses of local docker networks using this ipam
+   def local_docker_known_addresses
+    local_addresses = []
+
+    Docker::Network.all.each { |network|
+      debug "checking network #{network.json}"
+      nw_json = network.json
+      if nw_json.dig('IPAM', 'Driver') == 'kontena-ipam'
+        debug "Kontena ipam managed network, checking containers (#{nw_json.dig('Containers').size})"
+        nw_json.dig('Containers').each { |c, value|
+          debug "container: #{c}"
+          debug "value: #{value}"
+          address = IPAddr.new(value.dig('IPv4Address'))
+          local_addresses << address if address
+        }
+      end
+
+      local_addresses.map!{ |a| a.to_host }
+    }
+    info "Collected #{local_addresses.size} local Docker network addresses managed by Kontena ipam driver"
+    local_addresses
+   end
 
 end
