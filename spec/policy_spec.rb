c46977308226bc57e6d0363d847f9a668681b0bc
Tero Marttila <tero.marttila@kontena.io>
2016-11-23 12:31:11 +0200

    Edit Policy spec

--- spec/policy_spec.rb
+++ spec/policy_spec.rb
@@ -18,54 +18,59 @@ describe Policy do
   end
 
   context 'using the 10.80.0.0/12 supernet' do
-    let :policy do
+    subject do
       Policy.new(
         'KONTENA_IPAM_SUPERNET' => '10.80.0.0/12',
         'KONTENA_IPAM_SUBNET_LENGTH' => '24',
       )
     end
 
-    it 'allocates consecutive subnet' do
-      reserved = IPSet.new([
-        IPAddr.new('10.80.0.64/28'),
-      ])
+    describe '#allocatable_subnets' do
+      it 'allocates consecutive subnet' do
+        reserved = IPSet.new([
+          IPAddr.new('10.80.0.64/28'),
+        ])
 
-      expect(policy.allocatable_subnets(reserved).take(3)).to eq [
-          IPAddr.new('10.80.1.0/24'),
-          IPAddr.new('10.80.2.0/24'),
-          IPAddr.new('10.80.3.0/24'),
-      ]
+        expect(subject.allocatable_subnets(reserved).take(3)).to eq [
+            IPAddr.new('10.80.1.0/24'),
+            IPAddr.new('10.80.2.0/24'),
+            IPAddr.new('10.80.3.0/24'),
+        ]
+      end
+
+      it 'allocates after sparse subnets' do
+        expect(subject.allocatable_subnets(IPSet.new([
+          IPAddr.new('10.80.0.64/26'),
+          IPAddr.new('10.80.0.128/26'),
+        ])).first).to eq IPAddr.new('10.80.1.0/24')
+      end
+
+      it 'allocates after mixed subnets' do
+        expect(subject.allocatable_subnets(IPSet.new([
+          IPAddr.new('10.80.0.0/22'),
+          IPAddr.new('10.80.3.64/26'),
+        ])).first).to eq IPAddr.new('10.80.4.0/24')
+      end
+
+      it 'allocates in between subnets' do
+        expect(subject.allocatable_subnets(IPSet.new([
+          IPAddr.new('10.80.0.0/23'),
+          IPAddr.new('10.80.4.0/23'),
+        ])).first).to eq IPAddr.new('10.80.2.0/24')
+      end
+
+      it 'allocates to the very end' do
+        expect(subject.allocatable_subnets(IPSet.new([])).to_a.last).to eq IPAddr.new('10.95.255.0/24')
+      end
+
+      it 'returns nil if the supernet is full' do
+        reserved_subnets = (80..95).map {|x| IPAddr.new("10.#{x}.0.0/16") }
+
+        expect(subject.allocatable_subnets(IPSet.new(reserved_subnets)).first).to be_nil
+      end
     end
 
-    it 'allocates after sparse subnets' do
-      expect(policy.allocatable_subnets(IPSet.new([
-        IPAddr.new('10.80.0.64/26'),
-        IPAddr.new('10.80.0.128/26'),
-      ])).first).to eq IPAddr.new('10.80.1.0/24')
-    end
 
-    it 'allocates after mixed subnets' do
-      expect(policy.allocatable_subnets(IPSet.new([
-        IPAddr.new('10.80.0.0/22'),
-        IPAddr.new('10.80.3.64/26'),
-      ])).first).to eq IPAddr.new('10.80.4.0/24')
-    end
-
-    it 'allocates in between subnets' do
-      expect(policy.allocatable_subnets(IPSet.new([
-        IPAddr.new('10.80.0.0/23'),
-        IPAddr.new('10.80.4.0/23'),
-      ])).first).to eq IPAddr.new('10.80.2.0/24')
-    end
-
-    it 'allocates to the very end' do
-      expect(policy.allocatable_subnets(IPSet.new([])).to_a.last).to eq IPAddr.new('10.95.255.0/24')
-    end
-
-    it 'returns nil if the supernet is full' do
-      reserved_subnets = (80..95).map {|x| IPAddr.new("10.#{x}.0.0/16") }
-
-      expect(policy.allocatable_subnets(IPSet.new(reserved_subnets)).first).to be_nil
     end
   end
 end
